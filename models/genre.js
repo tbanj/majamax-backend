fcaf1dbeeae99c786d4a6e450354ce7ae26f81f1
Engr. Alabi Temitope Wahab <t.banji@rocketmail.com>
2019-09-07 20:33:34 +0100

    updated

--- models/genre.js
+++ models/genre.js
@@ -19,7 +19,7 @@ const Genre = mongoose.model('Genre', genreSchema);
 function validateGenre(genre) {
   const schema = {
     id: Joi.number().required(),
-    name: Joi.string().min(5).max(50).required()
+    name: Joi.string().min(3).max(50).required()
   };
 
   return Joi.validate(genre, schema);
